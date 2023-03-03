777a895e7192b19425c97f500eb1f1f658301db4
Michal Winiecki <falrond@gmail.com>
2023-03-03 13:04:02 +0100

    add prices section

--- assets/js/app.js
+++ assets/js/app.js
@@ -228,7 +228,7 @@ sr.reveal(".services__image-wrapper,.about__information, .post--left", {
 });
 
 sr.reveal(
-  ".services__list,.about__images, .post--left, .post--right, .projects-wrapper",
+  ".services__list,.about__images, .post--left, .post--right, .projects-wrapper, .prices__list",
   {
     origin: "right",
   }
@@ -370,3 +370,75 @@ btns[0].addEventListener("click", async (e) => {
 //   },
 //   50
 // );
+
+// --------------------------------------------------------------
+// 3d hover effect
+// --------------------------------------------------------------
+
+/* Store the element in el */
+// let el = document.getElementById("tilt");
+const elements = document.querySelectorAll(".tilt");
+
+/* Get the height and width of the element */
+
+/*
+ * Add a listener for mousemove event
+ * Which will trigger function 'handleMove'
+ * On mousemove
+ */
+
+elements.forEach((el) => {
+  const height = el.clientHeight;
+  const width = el.clientWidth;
+  el.addEventListener("mousemove", handleMove.bind(null, height, width, el));
+
+  el.addEventListener("mouseout", function () {
+    el.style.transform = "perspective(500px) scale(1) rotateX(0) rotateY(0)";
+  });
+
+  /* Add listener for mousedown event, to simulate click */
+  el.addEventListener("mousedown", function () {
+    el.style.transform = "perspective(500px) scale(0.9) rotateX(0) rotateY(0)";
+  });
+
+  /* Add listener for mouseup, simulate release of mouse click */
+  el.addEventListener("mouseup", function () {
+    el.style.transform = "perspective(500px) scale(1.1) rotateX(0) rotateY(0)";
+  });
+});
+/* Define function a */
+function handleMove(height, width, el, e) {
+  /*
+   * Get position of mouse cursor
+   * With respect to the element
+   * On mouseover
+   */
+  /* Store the x position */
+  const xVal = e.layerX;
+  /* Store the y position */
+  const yVal = e.layerY;
+
+  /*
+   * Calculate rotation valuee along the Y-axis
+   * Here the multiplier 20 is to
+   * Control the rotation
+   * You can change the value and see the results
+   */
+  const yRotation = 20 * ((xVal - width / 2) / width);
+
+  /* Calculate the rotation along the X-axis */
+  const xRotation = -20 * ((yVal - height / 2) / height);
+
+  /* Generate string for CSS transform property */
+  const string =
+    "perspective(500px) scale(1.1) rotateX(" +
+    xRotation +
+    "deg) rotateY(" +
+    yRotation +
+    "deg)";
+
+  /* Apply the calculated transformation */
+  el.style.transform = string;
+}
+
+/* Add listener for mouseout event, remove the rotation */
